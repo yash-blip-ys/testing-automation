7e65025e150b871bb7986772728cc596b96aa3e6
YUVRAJ SINGH <140778585+yash-blip-ys@users.noreply.github.com>
2026-09-09 12:41:07 +0530

    Add comments for Playwright script guidance
    Add comments for future Playwright script usage

---
+++ playwright_check.py
@@ -0,0 +1,2 @@
+#if any issue with the tool , it isnt able to autonomusly di the job here we can add the playwright script , to manually procede ,
+# in future updates will use this to provide guardrails and scope for the testing for extremely unique case or for rxtra precisssion .
